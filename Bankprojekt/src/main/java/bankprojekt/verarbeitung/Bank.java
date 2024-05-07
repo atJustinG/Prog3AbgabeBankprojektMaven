8dbad2036561f5682045a932d632db5172026890
atJustinG <45684351+atJustinG@users.noreply.github.com>
2024-05-07 15:05:51 +0200

    comments added

--- Bankprojekt/src/main/java/bankprojekt/verarbeitung/Bank.java
+++ Bankprojekt/src/main/java/bankprojekt/verarbeitung/Bank.java
@@ -6,35 +6,63 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Klasse Bank beschreibt das verhalten einer Bank die Konten verwaltet
+ * @author Justin Glowa
+ */
 public class Bank {
 
     private long bankleitzahl;
 
-    long Kontonummer = 0;
+    private long Kontonummer = 0;
 
-    HashMap<Long, Konto> giroKonten = new HashMap<Long,Konto>();
-    HashMap<Long, Konto> sparbuchKonten = new HashMap<Long, Konto>();
+    private HashMap<Long, Konto> giroKonten = new HashMap<Long,Konto>();
+    private HashMap<Long, Konto> sparbuchKonten = new HashMap<Long, Konto>();
 
+    /**
+     * Konstruktor zur erstellung eines Bankobjekts
+     * @param bankleitzahl Bankleitzahl der Bank
+     */
     public Bank(long bankleitzahl){
         this.bankleitzahl = bankleitzahl;
     }
 
+    /**
+     * getter für Bankleitzahl
+     * @return die aktuelle Bankleitzahl des Objekts
+     */
     public long getBankleitzahl(){
         return bankleitzahl;
     }
 
+    /**
+     * erstellen und hinzufügen eines Girokontos
+     * generiert eine neue Kontonummer bei jedem neu erstelltem Konto und speichert es in einer HashMap
+     * @param inhaber Kunde dem das Konto gegeben ist
+     * @return die Kontonummer des erstellten Kontos
+     */
     public long girokontoErstellen(Kunde inhaber){
             giroKonten.put(Kontonummer, new Girokonto(inhaber, Kontonummer, 500));
             Kontonummer++;
             return Kontonummer;
     }
 
-    public long sparbucherstellen(Kunde inhaber){
+    /**
+     * genauso wie giroKonto erstellt diese methode ein Sparbuch
+     * und vergibt ihm eine beliebige einzigartige Kontonummer.
+     * @param inhaber
+     * @return
+     */
+    public long sparbuchErstellen(Kunde inhaber){
         sparbuchKonten.put(Kontonummer, new Sparbuch(inhaber, Kontonummer));
         Kontonummer++;
         return Kontonummer;
     }
 
+    /**
+     * gibt alle Konten der Bank in einer Bank als Text zurück mit Konto und Kontostand
+     * @return zusammengebauten String aller Konten
+     */
     public String getAlleKonten(){
         String kontonummern = "";
         long tempKontonummer;
@@ -57,6 +85,10 @@ public class Bank {
         return kontonummern;
     }
 
+    /**
+     * gibt alle Kontonummern zurück als eine Liste
+     * @return Listenobjekt aller Kontonummern
+     */
     public List<Long> getAlleKontonummern(){
         ArrayList<Long> tempKontoNummern = new ArrayList<Long>(giroKonten.size());
         for (Map.Entry<Long, Konto> entry: giroKonten.entrySet()){
@@ -68,6 +100,13 @@ public class Bank {
         return tempKontoNummern;
     }
 
+    /**
+     * hebt Geld vom angegeben Konto ab unterscheidet anhand der Kontonummer die Kontoart
+     * @param von Kontonummer vom Konto von dem abgehoben werden soll
+     * @param betrag der Betrag der abgeheben werden soll
+     * @return boolean true wenn abheben erfolgreich ist false wenn das Konto nicht existiert
+     * @throws GesperrtException wird geworfen wenn das Konto gesperrt ist
+     */
     public boolean geldAbheben(long von, double betrag) throws GesperrtException {
         if(giroKonten.get(von) != null){
             return giroKonten.get(von).abheben(betrag);
@@ -76,6 +115,12 @@ public class Bank {
         }else return false;
     }
 
+    /**
+     * zahlt Geld auf ein Konto ein
+     * @param auf das Konto auf dem das Geld eingezahlt werden soll
+     * @param betrag der Betrag der eingezahlt werden soll
+     * @throws IllegalArgumentException wird bei einem falschen Betrag geworfen oder bei einer unbekannten Kontonummer
+     */
     public void geldEinzahlen(long auf, double betrag) throws IllegalArgumentException{
         if (giroKonten.get(auf) != null) {
             giroKonten.get(auf).einzahlen(betrag);
@@ -84,6 +129,11 @@ public class Bank {
         }else throw new IllegalArgumentException();
     }
 
+    /**
+     * gibt den aktuellen Kontostand des spezifizierten Kontos zurück
+     * @param nummer Kontonummer des Kontos
+     * @return den aktuellen Kontostand oder NaN bei einer falschen oder unbekannten kontonummer
+     */
     public double getKontostand(long nummer){
         if(giroKonten.get(nummer) != null){
             return giroKonten.get(nummer).getKontostand();
@@ -93,6 +143,15 @@ public class Bank {
         return Double.NaN;
     }
 
+    /**
+     * ueberweist einen Betrag von einem Konto auf ein anderes innerhalb der Bank
+     * @param vonKontonr Kontonummer des sendeden Kontos
+     * @param nachKontonr Kontonummer des empfangenden Kontos
+     * @param betrag Betrag der ueberwiesen werden soll
+     * @param verwendungszweck Verwendungszweck der Ueberweisung
+     * @return true bei durchgefuehrter Ueberweisung false wenn die Konten nicht existieren
+     * @throws GesperrtException wird geworfen wenn ein Konto gesperrt ist
+     */
     public boolean geldUeberweisen(long vonKontonr, long nachKontonr, double betrag, String verwendungszweck) throws GesperrtException {
         if(giroKonten.get(vonKontonr) != null && giroKonten.get(nachKontonr) != null){
             Girokonto giroKontoVon = (Girokonto) giroKonten.get(vonKontonr);
